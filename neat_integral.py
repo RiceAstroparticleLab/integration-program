a68674c241a703b2d1ccfd9080c74ba699836d03
zbilodea <70441641+zbilodea@users.noreply.github.com>
2023-02-24 11:25:16 -0500

    Add files via upload

--- neat_integral.py
+++ neat_integral.py
@@ -3,8 +3,10 @@ import numpy as np
 import math
 from numba import jit
 from math import log, pi
-import xarray as xr
+# import xarray as xr
 import zarr
+import scipy
+from scipy import special
 
 @jit(nopython=True)
 
@@ -48,7 +50,7 @@ def ramanujan_pmf(x, mu):
     y = nramanujan_logpmf(x,mu)
     return np.exp(y)
 
-def probability_array_gen(pmfs, interaction_positions, broken_count, pmf_lengths):
+def probability_array_gen(interaction_positions, broken_count, max_ks):
     """Instantiates an array of ones that is the correct shape
         for joint Poisson probability dist.
 
@@ -59,20 +61,18 @@ def probability_array_gen(pmfs, interaction_positions, broken_count, pmf_lengths
             one that is as long as the number of positions(?)
     """
 
-    dimension_of_grid = ([x for x in pmf_lengths]) #first dimension, to start
-    print(dimension_of_grid)
+    dimension_of_grid = ([max_ks[x] for x in range(broken_count)]) #first dimension, to start
     dimension_of_grid.append(interaction_positions)
+    # probabilities = zarr.array((np.nan(dimension_of_grid))) #change to fill with negative infinities
     probabilities = zarr.ones((dimension_of_grid))
-    print("Shape:", probabilities.shape)
-    print("Chunks", probabilities.chunks)
+    # probabilities *= numpy.nan
+    # probabilities[:] *= -math.inf
     return probabilities
 
-def calculate_univariate(k, mu, interaction_positions, radial_position):
+def calculate_univariate(mu, interaction_positions, radial_position):
     """
     Parameters
 	----------
-	k : integer
-		max k value to calculate the Poisson for
 	mu : float
 
 	Returns
@@ -82,55 +82,75 @@ def calculate_univariate(k, mu, interaction_positions, radial_position):
 
 	return pmf
     """
-    new_mus = np.array(interaction_positions, dtype=float)
-    max_ks = np.zeros(interaction_positions, dtype=int)
-    max_ks[0] = int(np.max(known_vals)/2)
-    new_mus[0] = mu
+    pmfs = []
+    max_k = 10 #ask Tina what to set this as!
     x = np.arange(0, max_k, 1)
-    pmf = np.array(ramanujan_pmf(x,mu))
+    pmf = np.array(nramanujan_logpmf(x,mu))
     for i in range(interaction_positions):
-        max_ks[i] = max_k
-        mu = (i + radial_positions[int(broken_sensor[i])]-1)/.25
-        new_mus[i] = mu
-        old_max_k = max_k
-        max_k += 10
-        while np.sum(pmf) <= 0.9999 and max_k < 400:
-            max_ks[i] = max_k #why?
+        max_k = 10
+        old_max_k = 0
+        x = np.arange(0, max_k, 1)
+        pmf = np.array(nramanujan_logpmf(x,mu))
+        while np.sum(pmf) <= 0.9999 and max_k < 100:
             x = np.arange(old_max_k, max_k, 1, dtype=int)
-            pmf = np.concatenate((pmf, ramanujan_pmf(x,mu))) #Look to see if this is a bottleneck later
+            pmf = np.concatenate((pmf, nramanujan_logpmf(x,mu))) #Look to see if this is a bottleneck later
             old_max_k = max_k
             max_k += 10
-        pmf_lengths[i] = len(pmf)
         pmfs.append(pmf)
+        #must make max_k the longest of the pmfs per interaction position
+    return pmfs, max_k
 
-    return pmfs, max_ks, new_mus
+def create_joint_dist(pmfs, broken_count, interaction_positions, probabilities, max_ks):
+    """
+    pmfs :
 
-def create_joint_dist(pmfs, broken_count, interaction_positions):
-    temp = zarr.ones(([len(p) for p in pmfs]), dtype=float)
-    shapes = np.ones([len(pmfs),len(pmfs)], dtype=int)
-    np.fill_diagonal(shapes, [len(p) for p in pmfs])
+    """
     for i in range(broken_count):
-        # for j in range(interaction_positions):
-        temp *= (np.array(pmfs[i])).reshape(shapes[i])
-    print("This is what I thought the joint distribution should look like:")
-    print(temp.shape)
-    dimension_of_grid = ([len(p) for p in pmfs]) #first dimension, to start
-    dimension_of_grid.append(len(positions))
-    probabilities *= np.expand_dims(temp,broken_count) #Doesn't work!
-    np.exp(probabilities)
-    print("Joint distribution in array with position dimension. Looks wrong.")
-    # probabilities = xr.DataArray(probabilities)
-#To sum over sensor axes
-    # probabilities = xr.DataArray(probabilities)
-    # xr.IndexVariable.sum(probabilities, dim='dim_'+str(broken_count))
-
+        for j in range(interaction_positions):
+            newaxes = [1] * broken_count
+            newaxes[i] = int(len(pmfs[i][j]))
+            # if values are finite, do this, else do that
+            # if probabilities[0,0,j] == nan:
+            #     probabilities[:,:,j] = np.array(pmfs[i][j]).reshape(newaxes)
+            # else:
+            probabilities[:,:,j] += np.array(pmfs[i][j]).reshape(newaxes)
     return probabilities
 
+            # probabilities[:,:,j] *= (np.array(pmfs[(i * interaction_positions) +j])).reshape(shapes[i]) #doing the reshaping to pmfs in temp
+
+
 def calculate_cross_term(mu1, mu2, cross_mu, max_k1, max_k2):
-    temp = -1*np.exp(cross_mu)
-    for i in range(max(max_k1,max_k2)):
-        temp += ((**(ramanujan(k[i])))*(**ramanujan(k[i])))
-    return temp *= **(ramanujan(k[i])) * (mus[broken_num1,broken_num2]/(mus[broken_num1]*mus[broken_num2]))
+    """
+    mu1 : float, mu value of same sensor max_k1 corresponds to
+    mu2 : float, mu value of same sensor max_k2 corresponds to
+    cross_mu : float
+    max_k1 : int
+    max_k2 : int
+
+    Returns
+    -------
+    float
+    """
+    temp = 0
+    temp1 = 0
+    first = 0
+    second = 0
+    third = 0
+    for p in range(min(max_k1,max_k2)):
+# Calculate cross term
+        # temp += np.exp(nr(max_k1) + nr(max_k2) - nr(p) - nr(max_k1 - p) - nr(max_k2 - p)) * (np.exp(nr(p)) * (cross_mu/(mu1*mu2)))
+        # print("Normal cross:", temp)
+# Modified cross term
+        temp1 += np.exp(1j*math.pi*min(max_k1,max_k2)) * ((cross_mu/(mu1*mu2))**min(max_k1,max_k2))*scipy.special.hyperu(-min(max_k1,max_k2),-min(max_k1,max_k2)+max(max_k1,max_k2)+1,-((mu1*mu2)/cross_mu))
+        first += np.exp(1j*math.pi*min(max_k1,max_k2))
+        second += ((cross_mu/(mu1*mu2))**min(max_k1,max_k2))
+        third += scipy.special.hyperu(-min(max_k1,max_k2),-min(max_k1,max_k2)+max(max_k1,max_k2)+1,-((mu1*mu2)/cross_mu))
+
+        print("First part", first)
+        print("Second part", second)
+        print("Third part", third)
+        print("New cross:", temp1)
+    return float(math.log(temp1)) + (cross_mu)
 
 
 def calculate_multivariate(cross_mus, interaction_positions, max_ks):
@@ -148,7 +168,7 @@ def calculate_multivariate(cross_mus, interaction_positions, max_ks):
     for j in range(interaction_positions):
         for i in range(broken_count): #best way probably depends on order of mus
             for p in range(broken_count):
-                probabilities[] *= ((-mu1[i,p]**max_ks[i,j])/**(ramanujan(max_ks[i,j]))
+                probabilities[i,p,j] *= ((-mu1[i,p]**max_ks[i,j])/np.exp(nr(max_ks[i,j])))
 
 def poisson_integration(known_vals, interaction_positions, radial_positions, cross_mus):
     """Integrates over a poisson distribution.
@@ -163,42 +183,52 @@ def poisson_integration(known_vals, interaction_positions, radial_positions, cro
         :var
         :return: array of probabilities integrated over position
     """
-    np.fill_diagonal(cross_mus,(interaction_positions + radial_positions[int(broken_sensor[x])]-1)/.25 for x, value in enumerate(cross_mus.diagonal()))
-    new_mus = np.array((cross_mus.diagonal()*(interaction_positions + radial_positions[int(broken_sensor[i])]-1)/.25), dtype=float)
-     = (interaction_positions + radial_positions[int(broken_sensor[i])]-1)/.25 #for all mus
-    broken_sensor = np.empty(7)
+
+    broken_sensor = []
     broken_count = 0
     for i, value in enumerate(known_vals):
         if value == -1:
-            broken_sensor[broken_count] = i
+            broken_sensor.append(i)
             broken_count += 1
 
-    # pmfs, max_ks, new_mus = calculate_univariate(mus, known_vals, interaction_positions, radial_positions)
-    # probabilities = probability_array_gen(pmfs, interaction_positions, broken_count, pmf_lengths)
-    max_ks = np.ones(broken_count)*400
-    #look for viable base-cases:first need k[1], k[0] is calculated in the first loop
-    univariate_poisson, max_ks[1] = calculate_univariate(cross_mus[1,1], interaction_positions, radial_position[1])
-    probabilities = np.ones(tuple(max_ks),interaction_positions)
+    #line below is meant to calculate new mus and correctly replace them in array cross_mus
+    # np.fill_diagonal(cross_mus,(interaction_positions + radial_positions[int(broken_sensor[x])]-1)/.25 for x, value in enumerate(cross_mus.diagonal()))
     for i in range(broken_count):
-        univariate_poisson, max_ks[i], cross_mus[i,i] = calculate_univariate(cross_mus[i,i], interaction_positions, radial_position[i]) #change to calculate log-probabilities
+        cross_mus[broken_sensor[i],broken_sensor[i]] = (interaction_positions + radial_positions[int(i)]-1)/.25
+
+    # pmfs, max_ks, cross_mus = calculate_univariate(np.diagonal(cross_mus), known_vals, interaction_positions, radial_positions)
+
+    max_ks = np.empty(broken_count)
+    univariate_poissons = []
+
+    for i in range(broken_count):
+        univariate_poisson, max_ks[i] = calculate_univariate(cross_mus[broken_sensor[i],broken_sensor[i]], interaction_positions, radial_positions[broken_sensor[i]]) #change to calculate log-probabilities
+        univariate_poissons.append(univariate_poisson)
         newaxes = [1] * broken_count
         newaxes[i] = [max_ks[i]]
-        probabilities *= np.expand_dims(univariate_poisson, axis=tuple(newaxes))
-        for j in np.arange(i+1, broken_count):
-            cross_term = calculate_cross_term(cross_mus[i,i], cross_mus[j,j], cross_mus[i,j], max_ks[i], max_ks[j])
-            newaxes = [1] * broken_count
-            newaxes[i] = [max_ks[i]]
-            newaxes[j] = [max_ks[j]]
-            probabilities *= np.expand_dims(cross_term, axis=tuple(newaxes))
-
-
-
+    univariate_poissons = np.array(univariate_poissons)
+    probabilities = probability_array_gen(interaction_positions, broken_count, max_ks) #save univariate_poissons to list to pass in for pmfs parameter
+    probabilities = np.exp(create_joint_dist(univariate_poissons, broken_count, interaction_positions, probabilities, max_ks))
     for i in range(broken_count):
-        probabilities = np.sum(probabilities,axis=0)
-        probabilities = probabilities/np.sum(probabilities) #?
-mus = np.array(([20,30,32],[31,12,8,][94,32,1]), dtype=float) #mu[0,0] = mu 0, mu[1,1] = mu 1... mu[0,1] = mu (0,1)...
+        for j in np.arange(i+1, broken_count):
+            cross_term = calculate_cross_term(cross_mus[broken_sensor[i],broken_sensor[i]], cross_mus[broken_sensor[j],broken_sensor[j]], cross_mus[broken_sensor[i],broken_sensor[j]], int(max_ks[i]), int(max_ks[j]))
+            newaxes = [1] * broken_count
+            newaxes[i] = int(max_ks[i])
+            newaxes[j] = int(max_ks[j])
+            print("cross", cross_term)
+            # probabilities[i,j] *= np.array(cross_term).reshape(newaxes)
+            probabilities[i,j] *= np.array(cross_term)
+
+ # np.array(pmfs[i][j]).reshape(newaxes)
+#calculate integral
+    for i in range(broken_count):
+        probabilities = np.sum(probabilities,axis=-1)
+        probabilities = probabilities/np.sum(probabilities) #renormalizing
+    return probabilities
+
+mus = np.array(([20,30,32],[31,12,8],[94,32,1]), dtype=float) #mu[0,0] = mu 0, mu[1,1] = mu 1... mu[0,1] = mu (0,1)...
 interaction_positions = 5
 radial_positions = np.array([4,5,7])
 # base_mu_vals= np.array([6,4,7])
 known_vals = np.array([13,-1,-1])
-poisson_integration(known_vals, interaction_positions, radial_positions, mus)
+print(poisson_integration(known_vals, interaction_positions, radial_positions, mus))
